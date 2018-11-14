653a5634138569e06588013b52fb896e73993a50
Adones Pitogo <pitogo.adones@gmail.com>
2018-11-14 19:04:01 +0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const { task, src, dest, start, series } = require('gulp')
+const concat = require('gulp-concat')
+const ngTemplates = require('gulp-angular-templatecache')
+const del = require('del')
+
+function clean() {
+  return del([
+    'dist'
+  ]);
+}
+
+function templates() {
+  return src('./src/**/*.html')
+    .pipe(ngTemplates({
+      module: 'ado.timer-settings.tpls',
+      standalone: true
+    }))
+    .pipe(dest('.tmp'))
+}
+
+function build() {
+  return src(['.tmp/**/*.js', 'src/**/*/js'])
+    .pipe(concat('ado-ng-timer-settings.js'))
+    .pipe(dest('./dist'))
+}
+
+task('default', series(clean, templates, build))
+
+
