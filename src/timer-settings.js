87f485a910c77b079ceb6f2327c11cf1d377d50f
Adones Pitogo <pitogo.adones@gmail.com>
2018-12-08 18:26:09 +0800

    v0.1.12

--- src/timer-settings.js
+++ src/timer-settings.js
@@ -48,6 +48,20 @@
           $ctrl.allow_pause_validity = res.data.allow_pause_validity / (60*60);
           $ctrl.allow_pause_time = res.data.allow_pause_time / (60*60);
           $ctrl.pins = model_pins[res.data.hardware.model];
+
+          var model = $ctrl.settings.hardware.model;
+          var versions = [{desc:'With Resistor(v1)', val:1}, {desc:'Without Resistor(v2)', val:2}, {desc: 'BCM GPIO', val: 3}, {desc: 'Serial Connection', val:4}];
+
+          if (model == 'raspberry_pi_3') {
+            return versions.filter(function (v) {
+              return v.val == 1 || v.val == 2 || v.val == 4;
+            });
+          } else {
+            return versions.filter(function (v) {
+              return v.val == 3 || v.val == 4;
+            });
+          }
+
         }
 
         function update(timer) {
@@ -94,20 +108,9 @@
           }
         };
 
-        $ctrl.coinslot_versions = function (settings) {
-          var model = settings.hardware.model;
-          var versions = [{desc:'With Resistor(v1)', val:1}, {desc:'Without Resistor(v2)', val:2}, {desc: 'BCM GPIO', val: 3}, {desc: 'Serial Connection', val:4}];
+        //$ctrl.coinslot_versions = function (settings) {
 
-          if (model == 'raspberry_pi_3') {
-            return versions.filter(function (v) {
-              return v.val == 1 || v.val == 2 || v.val == 4;
-            });
-          } else {
-            return versions.filter(function (v) {
-              return v.val == 3 || v.val == 4; 
-            });
-          }
-        };
+        //};
 
       };
 
